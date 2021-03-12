f824fbe1aaf5a1e214f0fcef5d742d140c468e29
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2021-03-12 16:55:45 +0100

    initial commit

---
+++ dicomtrolley/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for dicomtrolley."""
+
+__author__ = """Sjoerd Kerkstra"""
+__email__ = 'sjoerd.kerkstra@radboudumc.nl'
+__version__ = '0.1.0'
---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use dicomtrolley in a project::
+
+    import dicomtrolley
